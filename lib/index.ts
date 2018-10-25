e75d6ba0c666ac5e946a24c53efcf6cc99d04508
Steve Thompson <writetome51@gmail.com>
2018-10-25 16:36:59 -0600

    testing out using .d.ts file

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,30 @@
-import {PublicArrayContainer} from './PublicArrayContainer';
+import { IPublicDataContainer } from 'public-data-container-interface/IPublicDataContainer';
+import { errorIfNotArray } from 'basic-data-handling/errorIfNotArray';
+import { BatchGetterSetter } from 'batch-getter-setter/BatchGetterSetter';
+
+
+export abstract class PublicArrayContainer extends BatchGetterSetter implements IPublicDataContainer<any[]> {
+
+
+	constructor(private _data: any[]) {
+		super();
+		errorIfNotArray(this._data);
+	}
+
+
+	get data() {
+		return this._data;
+	}
+
+
+	// Every time this.data is assigned value, it will be checked to see if it's an
+	// array:
+	set data(value) {
+		errorIfNotArray(value);
+		this._data = value;
+	}
+
+
+}
+// test comments
+// aaaaa
